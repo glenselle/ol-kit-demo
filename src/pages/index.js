43fdc0d297cd58f8477aeab553694ef1c1bb6579
Glen Selle <glen.selle.ext@bayer.com>
2020-05-14 15:43:50 -0500

    Initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
-import React from "react"
+import React from 'react'
+import { Map, Popup, Controls, centerAndZoom } from '@bayer/ol-kit'
 
-export default () => <div>Hello world!</div>
+import VectorLayer from 'ol/layer/vector'
+import VectorSource from 'ol/source/vector'
+
+class App extends React.Component {
+  onMapInit = map => {
+    const data = new VectorLayer({
+      source: new VectorSource({
+        features: [/** get some data and have fun with it */]
+      })
+    })
+    // add the data to the map
+    map.addLayer(data)
+
+    // quickly take the map
+    centerAndZoom(map, {x: 0, y: 0, zoom: 3})
+  }
+
+  render () {
+    return (
+      <Map onMapInit={this.onMapInit}>
+        <Popup />
+        <Controls />
+      </Map>
+    )
+  }
+}
+
+export default App
