edbb772f2fbeabc0c302781a65ba7489938cb30d
Daniel Rodenberg <daniel.rodenberg@monsanto.com>
2020-05-20 14:49:18 -0500

    added layerstyler and basemap selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,34 @@
 import React from 'react'
-import { Map, LayerPanel, Controls, centerAndZoom, Popup } from '@bayer/ol-kit'
-import BasemapContainer from './BasemapContainer'
+import {
+  Map,
+  LayerPanel,
+  Controls,
+  Popup,
+  BasemapContainer,
+  LayerStyler,
+  LayerPanelPage,
+  LayerPanelContent
+} from '@bayer/ol-kit'
+import BorderStyleIcon from '@material-ui/icons/BorderStyle';
 
 import "./app.css"
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      map: {}
-    }
-  }
-
-  onMapInit = map => {
-    this.setState({ map })
-    // quickly take the map
-    centerAndZoom(map, {x: 0, y: 0, zoom: 3})
-  }
-
   render () {
-    if (this.state.map) {
-      return (
-        <Map onMapInit={this.onMapInit} fullScreen>
-          <Popup />
-          <LayerPanel />
-          <Controls variation={'dark'} />
-          <BasemapContainer map={this.state.map} />
-        </Map>
-      )
-    } else {
-      return null
-    }
-    
+    return (
+      <Map fullScreen>
+        <Popup />
+        <LayerPanel>
+          <LayerPanelPage tabIcon={<BorderStyleIcon />}>
+            <LayerPanelContent style={{ padding: '0px' }}>
+              <LayerStyler />
+            </LayerPanelContent>
+          </LayerPanelPage>
+        </LayerPanel>
+        <Controls variation={'dark'} />
+        <BasemapContainer />
+      </Map>
+    )
   }
 }
 
