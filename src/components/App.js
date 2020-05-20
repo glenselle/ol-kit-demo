3e20d419549ab236be9793fb24569d603a59d5ea
Daniel Rodenberg <daniel.rodenberg@monsanto.com>
2020-05-20 15:55:58 -0500

    added diltzs lake house

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,35 @@ import {
   BasemapContainer,
   LayerStyler,
   LayerPanelPage,
-  LayerPanelContent
+  LayerPanelContent,
+  VectorLayer,
+  centerAndZoom
 } from '@bayer/ol-kit'
-import BorderStyleIcon from '@material-ui/icons/BorderStyle';
+import BorderStyleIcon from '@material-ui/icons/BorderStyle'
+import olSourceVector from 'ol/source/vector'
+import olFeature from 'ol/feature'
+import olGeomPoint from 'ol/geom/point'
+import olProj from 'ol/proj'
 
 import "./app.css"
 
 class App extends React.Component {
+  onMapInit = (map) => {
+    var layer = new VectorLayer({
+      title: 'Diltz\'s House',
+      source: new olSourceVector({
+        features: [new olFeature({ ['x marks the spot']: ['the lake house'], geometry: new olGeomPoint(olProj.fromLonLat([-89.940598,38.923107])) })]
+      })
+    })
+
+    map.addLayer(layer)
+
+    centerAndZoom(map, { x: -89.941642, y: 38.922929, zoom: 17.20 })
+  }
+
   render () {
     return (
-      <Map fullScreen>
+      <Map onMapInit={this.onMapInit} fullScreen>
         <Popup />
         <LayerPanel>
           <LayerPanelPage tabIcon={<BorderStyleIcon />}>
